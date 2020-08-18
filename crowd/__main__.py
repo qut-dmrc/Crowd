8ad19911330979f02c16114444517625c087dfd6
xueyingtan <xueyingt96@gmail.com>
2020-08-19 09:59:50 +1000

    Fixed paging error and support log file

--- crowd/__main__.py
+++ crowd/__main__.py
@@ -1,10 +1,13 @@
 import datetime
 import click
 from socialreaper.apis import API
-from socialreaper.tools import CSV
+# from socialreaper.tools import CSV
 from .crowdapi import CrowdTangle
+from .tools import CSV
 from backports.datetime_fromisoformat import MonkeyPatch
 MonkeyPatch.patch_fromisoformat()
+import logging
+logging.basicConfig(filename='paging.log',level=logging.INFO)
 
 @click.command()
 @click.argument('endpoint', nargs=1, type=click.Choice(['posts', 'posts/search'], case_sensitive=False), required=True)
@@ -14,7 +17,8 @@ MonkeyPatch.patch_fromisoformat()
 @click.option('--start_date', nargs=1, type=datetime.datetime.fromisoformat)
 @click.option('--end_date', nargs=1, type=datetime.datetime.fromisoformat, default=datetime.datetime.now().isoformat())
 @click.option('--output_filename', nargs=1, default="{}.csv".format(datetime.datetime.now().replace(microsecond=0).isoformat().replace(":",'.')))
-def main(token, endpoint, lists, search_terms, start_date, end_date, output_filename):
+@click.option('--offset', nargs=1, default=0)
+def main(token, endpoint, lists, search_terms, start_date, end_date, output_filename, offset):
     timeFrames = getTimeframeList(start_date, end_date)
     if lists:
         inListIds = lists.strip().replace(" ","")
@@ -26,16 +30,18 @@ def main(token, endpoint, lists, search_terms, start_date, end_date, output_file
         ct = CrowdTangle(token)
         print("Retrieving from {} to {}".format(start,end))
         if endpoint=='posts/search':
-            res = ct.postSearch(search_term=search_terms, inListIds=inListIds, start_date=start, end_date=end)
+            res = ct.postSearch(search_term=search_terms, inListIds=inListIds, start_date=start, end_date=end, offset=offset)
             if res['result'] and res['result']['posts']:
                 # if actualstartDate in res['result']:
                 #     nextEndDate
                 CSV(list(res['result']['posts']), file_name=output_filename, append=True)
                 while 'nextPage' in res['result']['pagination']:
                     nextPage = res['result']['pagination']['nextPage']
+                    logging.info(nextPage)
                     # retrieve next page end point and params
                     nextPageParams = nextPage.replace("https://api.crowdtangle.com/", "")
-                    res = ct.api_call(nextPageParams,{"searchTerm": search_terms})
+                    res = ct.api_call(nextPageParams,"")
+                    
                     CSV(list(res['result']['posts']), file_name=output_filename, append=True)
         else:
             print('only posts/search is supported')
@@ -50,7 +56,7 @@ def getTimeframeList(startDate, endDate):
         timeList.append([timeFrameStart.isoformat(), endDate.isoformat()])
         endDate = timeFrameStart-datetime.timedelta(seconds=1)
     timeList.append([startDate.isoformat(), endDate.isoformat()])
-    return timeList;
+    return timeList
 
 if __name__ == '__main__':
     main()
