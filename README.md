753cda76140f4470ea933fe9cf54c7f1cfd7dfd4
xueyingtan <xueyingt96@gmail.com>
2020-08-19 18:38:00 +1000

    CSV writing to disk issue fixed, log switch

--- README.md
+++ README.md
@@ -20,18 +20,21 @@ Commands:
   crowd "post/search" [OPTIONS]      searches the entire, cross-platform CrowdTangle system of posts
 
 OPTIONS
-  --token           The API token associated with dashboard                                         [string] [required]
-  --lists           A comma-separated list of the IDs of lists to search within.               [string] [default: null]
-  --search_terms    Retrieve posts which contain the search terms                              [string] [default: null]
-  --start_date      Filter posts ranging from start_date (e.g "2020-08-08")         [string] [default:end_date-365days]
-  --end_date        Filter posts ranging to end_date (e.g "2020-08-08")                         [string] [default: now]
-  --output_filename File to write/append to with csv extension only ('result.csv')  [string][default:<currentTime>.csv]
-  --offset          Posts offset for pagination purpose                                               [int][default: 0] 
+  -token/--token       The API token associated with dashboard                                         [string] [required]
+  -ls/--lists          A comma-separated list of the IDs of lists to search within.               [string] [default: null]
+  -s/--search_terms    Retrieve posts which contain the search terms                              [string] [default: null]
+  -sdate/--start_date  Filter posts ranging from start_date (e.g "2020-08-08")         [string] [default:end_date-365days]
+  sedate/--end_date    Filter posts ranging to end_date (e.g "2020-08-08")                         [string] [default: now]
+  --output_filename    File to write/append to with csv extension only ('result.csv')  [string][default:<currentTime>.csv]
+  -off/--offset        Posts offset for pagination purpose                                               [int][default: 0] 
+  -log/--log           Turning on logging option                                                    [bool][default: False] 
   --help
 
 Examples:
-  crowd "posts/search" --token="<API_TOKEN>"     Download all the available posts from posts/search endpoint
-  --lists="123,456" --search_terms="kw1,kw2"     restricted to lists IDs 123 and 456 matching keywords kw1 or kw2
-  --offset=500                                   offset 500 to access page 6 (each page returns maximum 100 posts)
+  crowd "posts/search" -token="<API_TOKEN>"        Download all the available posts from posts/search endpoint
+  -ls="123,456" -s="kw1,kw2"                       Restricted to lists IDs 123 and 456 matching keywords kw1 or kw2
+  -off=500                                         Offset 500 to access page 6 (each page returns maximum 100 posts)
+  -sdate="2020-06-01" -edate="2020-08-20"          Start and end dates
+  -log                                             Turn on logging
 
 Source code available at https://github.com/qut-dmrc/Crowd.git
--- crowd/__main__.py
+++ crowd/__main__.py
@@ -1,25 +1,28 @@
 import datetime
 import click
+import csv
 from socialreaper.apis import API
-from socialreaper.tools import CSV
+from socialreaper.tools import flatten,fill_gaps
 from .crowdapi import CrowdTangle
 # from .tools import CSV
 from backports.datetime_fromisoformat import MonkeyPatch
 MonkeyPatch.patch_fromisoformat()
 import logging
-logging.basicConfig(filename='paging.log',level=logging.INFO)
 
 @click.command()
 @click.argument('endpoint', nargs=1, type=click.Choice(['posts', 'posts/search'], case_sensitive=False), required=True)
-@click.option('--token', nargs=1, required=True)
-@click.option('--lists', nargs=1)
-@click.option('--search_terms', nargs=1, default="")
-@click.option('--start_date', nargs=1, type=datetime.datetime.fromisoformat)
-@click.option('--end_date', nargs=1, type=datetime.datetime.fromisoformat, default=datetime.datetime.now().isoformat())
+@click.option('-token','--token', nargs=1, required=True)
+@click.option('-ls','--lists', nargs=1)
+@click.option('-s','--search_terms', nargs=1, default="")
+@click.option('-sdate','--start_date', nargs=1, type=datetime.datetime.fromisoformat)
+@click.option('-edate','--end_date', nargs=1, type=datetime.datetime.fromisoformat, default=datetime.datetime.now().isoformat())
 @click.option('--output_filename', nargs=1, default="{}.csv".format(datetime.datetime.now().replace(microsecond=0).isoformat().replace(":",'.')))
-@click.option('--offset', nargs=1, default=0)
-def main(token, endpoint, lists, search_terms, start_date, end_date, output_filename, offset):
+@click.option('-off','--offset', nargs=1, default=0)
+@click.option('-log','--log', 'log', flag_value=True, default=False)
+def main(token, endpoint, lists, search_terms, start_date, end_date, output_filename, offset, log):
     timeFrames = getTimeframeList(start_date, end_date)
+    if log:
+        logging.basicConfig(filename='paging.log',level=logging.INFO)
     if lists:
         inListIds = lists.strip().replace(" ","")
     else:
@@ -34,15 +37,24 @@ def main(token, endpoint, lists, search_terms, start_date, end_date, output_file
             if res['result'] and res['result']['posts']:
                 # if actualstartDate in res['result']:
                 #     nextEndDate
-                CSV(list(res['result']['posts']), file_name=output_filename, append=True)
-                while 'nextPage' in res['result']['pagination']:
-                    nextPage = res['result']['pagination']['nextPage']
-                    logging.info(nextPage)
-                    # retrieve next page end point and params
-                    nextPageParams = nextPage.replace("https://api.crowdtangle.com/", "")
-                    res = ct.api_call(nextPageParams,"")
-                    
-                    CSV(list(res['result']['posts']), file_name=output_filename, append=True)
+                # flatten dictionary and fill gap
+                fieldnames, data = fill_gaps([flatten(datum) for datum in list(res['result']['posts'])])
+                with open(output_filename, 'a', encoding='utf-8', errors='ignore', newline='') as f:
+                # CSV(list(res['result']['posts']), file_name=output_filename, append=True)
+                    writer = csv.DictWriter(f, fieldnames=fieldnames, lineterminator='\n', extrasaction='ignore')
+                    writer.writeheader()
+                    writer.writerows(data)
+                    while 'nextPage' in res['result']['pagination']:
+                        nextPage = res['result']['pagination']['nextPage']
+                        if log:
+                            logging.info(nextPage)
+                        # retrieve next page end point and params
+                        nextPageParams = nextPage.replace("https://api.crowdtangle.com/", "")
+                        res = ct.api_call(nextPageParams,"")
+                        
+                        fieldnames, data = fill_gaps([flatten(datum) for datum in list(res['result']['posts'])])
+                        writer.writerows(data)
+                        # CSV(list(res['result']['posts']), file_name=output_filename, append=True)
         else:
             print('only posts/search is supported')
 
