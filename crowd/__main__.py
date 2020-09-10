d56a8a170554125ae6caecf3754544e504f71625
Jane Tan <tanx5@qut.edu.au>
2020-09-10 14:52:22 +1000

    uncomment code

--- crowd/__main__.py
+++ crowd/__main__.py
@@ -9,7 +9,6 @@ import numpy as np
 from socialreaper.apis import API
 from socialreaper.tools import flatten,fill_gaps
 from .crowdapi import CrowdTangle
-# from .tools import CSV
 from backports.datetime_fromisoformat import MonkeyPatch
 MonkeyPatch.patch_fromisoformat()
 
@@ -65,21 +64,7 @@ def main(config, token, lists, search_terms, and_terms, not_terms, start_date, e
         start_date = datetime.datetime.fromisoformat(start_date)
     if end_date:
         end_date = datetime.datetime.fromisoformat(end_date)
-    
-    # print(start_date, type(start_date))
-    # print(end_date, type(end_date))
-    # print(token, type(token))
-    # print(lists, type(lists))
-    # print(search_terms, type(search_terms))
-    # print(output_filename, type(output_filename))
-    # print(offset, type(offset))
-    # print(log, type(log))
-    # print(links, type(links))
-    # print(and_terms, type(and_terms))
-    # print(not_terms, type(not_terms))
 
-
-    timeFrames = getTimeframeList(start_date, end_date)
     if log:
         logging.basicConfig(filename='paging.log',level=logging.INFO)
     if lists:
@@ -88,55 +73,22 @@ def main(config, token, lists, search_terms, and_terms, not_terms, start_date, e
         inListIds = None
     actualStartDate = end_date
     prevActualStartDate = None
-    # result = pd.read_csv('russia-june.csv')
-    # prevActualStartDate = None
-    # actualStartDate = pd.DataFrame(result).iloc[-1].at['date']
-    # actualStartDate = datetime.datetime.fromisoformat(actualStartDate)
-    # if actualStartDate>start_date:
-    #     end_date = actualStartDate
-    # while(not start_date or actualStartDate>start_date and actualStartDate != prevActualStartDate):
-    timeFrames = getTimeframeList(start_date, actualStartDate)
-    for timeframe in timeFrames:
-        start = timeframe[0]
-        end = timeframe[1]
-        ct = CrowdTangle(token)
-        print("Retrieving from {} to {}".format(start,end))
-        if endpoint=='posts/search':
-            res = ct.postSearch(search_term=search_terms, and_kw=and_terms, not_kw=not_terms, inListIds=inListIds, start_date=start, end_date=end, offset=offset)
-            if res['result'] and res['result']['posts']:
-                # if actualstartDate in res['result']:
-                #     nextEndDate
-                # flatten dictionary and fill gap
-                fieldnames, data = fill_gaps([flatten(datum) for datum in list(res['result']['posts'])])
-                with open(output_filename, 'a', encoding='utf-8', errors='ignore', newline='') as f:
-                # CSV(list(res['result']['posts']), file_name=output_filename, append=True)
-                    writer = csv.DictWriter(f, fieldnames=fieldnames, lineterminator='\n', extrasaction='ignore')
-                    writer.writeheader()
-                    writer.writerows(data)
-                    while 'nextPage' in res['result']['pagination']:
-                        nextPage = res['result']['pagination']['nextPage']
-                        if log:
-                            logging.info(nextPage)
-                        # retrieve next page end point and params
-                        nextPageParams = nextPage.replace("https://api.crowdtangle.com/", "")
-                        res = ct.api_call(nextPageParams,"")
-                        
-                        fieldnames, data = fill_gaps([flatten(datum) for datum in list(res['result']['posts'])])
-                        writer.writerows(data)
-                        # CSV(list(res['result']['posts']), file_name=output_filename, append=True)
-        elif endpoint=="links":
-            for i in range(len(links)):
-                res = ct.links(link=links[i], start_date=start, end_date=end, offset=offset)
+
+    while(not start_date or actualStartDate>start_date and actualStartDate != prevActualStartDate):
+        timeFrames = getTimeframeList(start_date, actualStartDate)
+        for timeframe in timeFrames:
+            start = timeframe[0]
+            end = timeframe[1]
+            ct = CrowdTangle(token)
+            print("Retrieving from {} to {}".format(start,end))
+            if endpoint=='posts/search':
+                res = ct.postSearch(search_term=search_terms, and_kw=and_terms, not_kw=not_terms, inListIds=inListIds, start_date=start, end_date=end, offset=offset)
                 if res['result'] and res['result']['posts']:
-                    # if actualstartDate in res['result']:
-                    #     nextEndDate
                     # flatten dictionary and fill gap
                     fieldnames, data = fill_gaps([flatten(datum) for datum in list(res['result']['posts'])])
                     with open(output_filename, 'a', encoding='utf-8', errors='ignore', newline='') as f:
-                    # CSV(list(res['result']['posts']), file_name=output_filename, append=True)
                         writer = csv.DictWriter(f, fieldnames=fieldnames, lineterminator='\n', extrasaction='ignore')
-                        if i == 0:
-                            writer.writeheader()
+                        writer.writeheader()
                         writer.writerows(data)
                         while 'nextPage' in res['result']['pagination']:
                             nextPage = res['result']['pagination']['nextPage']
@@ -148,19 +100,41 @@ def main(config, token, lists, search_terms, and_terms, not_terms, start_date, e
                             
                             fieldnames, data = fill_gaps([flatten(datum) for datum in list(res['result']['posts'])])
                             writer.writerows(data)
-        else:
-            print('End point {} not supported'.format(endpoint))
-        # # getting actual start date
-        # prevActualStartDate = actualStartDate 
-        # header = pd.read_csv(output_filename, nrows=1)
-        # headers = header.columns.values
-        # dateIndex = np.where( headers=='date')
-        # with open(output_filename,'r',encoding='utf-8') as result:
-        #     last_line = result.readlines()[-1].strip().split(',')
-        #     actualStartDate = np.array(last_line)[dateIndex][0]
-        #     actualStartDate = datetime.datetime.fromisoformat(actualStartDate)
-        # if actualStartDate>start_date:
-        #     end_date = actualStartDate
+            elif endpoint=="links":
+                for i in range(len(links)):
+                    res = ct.links(link=links[i], start_date=start, end_date=end, offset=offset)
+                    if res['result'] and res['result']['posts']:
+                        # flatten dictionary and fill gap
+                        fieldnames, data = fill_gaps([flatten(datum) for datum in list(res['result']['posts'])])
+                        with open(output_filename, 'a', encoding='utf-8', errors='ignore', newline='') as f:
+                        # CSV(list(res['result']['posts']), file_name=output_filename, append=True)
+                            writer = csv.DictWriter(f, fieldnames=fieldnames, lineterminator='\n', extrasaction='ignore')
+                            if i == 0:
+                                writer.writeheader()
+                            writer.writerows(data)
+                            while 'nextPage' in res['result']['pagination']:
+                                nextPage = res['result']['pagination']['nextPage']
+                                if log:
+                                    logging.info(nextPage)
+                                # retrieve next page end point and params
+                                nextPageParams = nextPage.replace("https://api.crowdtangle.com/", "")
+                                res = ct.api_call(nextPageParams,"")
+                                
+                                fieldnames, data = fill_gaps([flatten(datum) for datum in list(res['result']['posts'])])
+                                writer.writerows(data)
+            else:
+                print('End point {} not supported'.format(endpoint))
+        # getting actual start date
+        prevActualStartDate = actualStartDate 
+        header = pd.read_csv(output_filename, nrows=1)
+        headers = header.columns.values
+        dateIndex = np.where( headers=='date')
+        with open(output_filename,'r',encoding='utf-8') as result:
+            last_line = result.readlines()[-1].strip().split(',')
+            actualStartDate = np.array(last_line)[dateIndex][0]
+            actualStartDate = datetime.datetime.fromisoformat(actualStartDate)
+        if actualStartDate>start_date:
+            end_date = actualStartDate
 
 # split timeframe into a list of timeframes with each is of maximum one year margin
 def getTimeframeList(startDate, endDate):
