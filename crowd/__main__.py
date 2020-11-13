93321eb0c7b7f10b3f879862b8079af4b586fca8
betsy <elizabeth.alpert@qut.edu.au>
2020-11-13 13:23:51 +1000

    PEP8 formatting for __main__.py

--- crowd/__main__.py
+++ crowd/__main__.py
@@ -1,12 +1,17 @@
 import click
 from .crowdapi import CrowdTangle
 
+
 @click.command()
 @click.option('-c','--config', nargs=1, default="default_query.yml")
 @click.option('-a','--append', 'append', flag_value=True)
 def main(config, append):
     ct = CrowdTangle(config, append)
+@click.option('-c', '--config', nargs=1, default="default_query.yml")
+def main(config):
+    ct = CrowdTangle(config)
     ct.run()
+
 
 if __name__ == '__main__':
     main()
