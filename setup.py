5d59be7c9f648aeb68b8cd4aad81989f596b14ca
betsy <elizabeth.alpert@qut.edu.au>
2020-11-17 16:24:54 +1000

    Add Pytest to requirements

--- setup.py
+++ setup.py
@@ -4,12 +4,19 @@ install_requires = [
     "google-cloud-bigquery"
 ]
 
+extras_require = {
+    "development": [
+        "pytest"
+    ]
+}
+
 setup(
     name='crowd',
     version='0.1.0',
     packages=['crowd'],
     python_requires=">=3.8",
     install_requires=install_requires,
+    extras_require=extras_require,
     entry_points={
         'console_scripts': [
             'crowd = crowd.__main__:main'
