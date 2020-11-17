e6d077a00b20e55c90a7259a6b40c10d99cb4064
betsy <elizabeth.alpert@qut.edu.au>
2020-11-17 14:58:07 +1000

    Fix incorrect merge in __main__.py

--- crowd/__main__.py
+++ crowd/__main__.py
@@ -3,13 +3,10 @@ from .crowdapi import CrowdTangle
 
 
 @click.command()
-@click.option('-c','--config', nargs=1, default="default_query.yml")
-@click.option('-a','--append', 'append', flag_value=True)
+@click.option('-c', '--config', nargs=1, default="default_query.yml")
+@click.option('-a', '--append', 'append', flag_value=True)
 def main(config, append):
     ct = CrowdTangle(config, append)
-@click.option('-c', '--config', nargs=1, default="default_query.yml")
-def main(config):
-    ct = CrowdTangle(config)
     ct.run()
 
 
