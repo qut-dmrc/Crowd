181bd5ea9e036eff20db11c71bf3eb64df22fb1d
Jane Tan <xueyingt96@gmail.com>
2021-04-27 09:10:11 +1000

    togbq batch upload script skip uploaded rows by n

--- togbq_batch.py
+++ togbq_batch.py
@@ -14,40 +14,50 @@ tablename="table_name"
 
 # all the job ids to be pusehd to BQ(crowdtangle)
 job_ids = [
+    # "true"
 ]
-bq_key = "" #sevice account key to push to BQ
+bq_key = "dmrc-data-26d311ddd0bf.json" #sevice account key to push to BQ
 # # #store to csv
 for job_id in job_ids:
     mode = 'w'
     header = True
     chunksize = 50000
-    for chunk in pd.read_sql_query("SELECT * FROM "+tablename+" WHERE _jobEntryDateTime=\""+job_id+"\"", conn, chunksize=chunksize):
+    for chunk in pd.read_sql_query("SELECT * FROM "+tablename, conn, chunksize=chunksize):
+    # for chunk in pd.read_sql_query("SELECT * FROM "+tablename+" WHERE _jobEntryDateTime=\""+job_id+"\"", conn, chunksize=chunksize):
         chunk = chunk[chunk.columns.difference(['_pushedToBQ','_jobEntryDateTime','_searchTermLink'])]
         chunk = chunk[chunk['platformId'] != 'platformId']
         chunk.to_csv("temp.csv", index=False)
         append_to_bq(bq_key, "crowdtangle."+tablename, "temp.csv", schema=main_data_schema)
     
-    for chunk in pd.read_sql_query("SELECT * FROM "+tablename+"_media WHERE _jobEntryDateTime=\""+job_id+"\"", conn, chunksize=chunksize):
+    for chunk in pd.read_sql_query("SELECT * FROM "+tablename+"_media", conn, chunksize=chunksize):
+    # for chunk in pd.read_sql_query("SELECT * FROM "+tablename+"_media WHERE _jobEntryDateTime=\""+job_id+"\"", conn, chunksize=chunksize):
         chunk = chunk[chunk.columns.difference(['_pushedToBQ','_jobEntryDateTime','_searchTermLink'])]
         chunk = chunk[chunk['platformId'] != 'platformId']
         chunk.to_csv("temp.csv", index=False)
         append_to_bq(bq_key, "crowdtangle."+tablename+"_media", "temp.csv", schema=media_schema)
 
-    for chunk in pd.read_sql_query("SELECT * FROM "+tablename+"_expandedLinks WHERE _jobEntryDateTime=\""+job_id+"\"", conn, chunksize=chunksize):
+    for chunk in pd.read_sql_query("SELECT * FROM "+tablename+"_expandedLinks", conn, chunksize=chunksize):
+    # for chunk in pd.read_sql_query("SELECT * FROM "+tablename+"_expandedLinks WHERE _jobEntryDateTime=\""+job_id+"\"", conn, chunksize=chunksize):
         chunk = chunk[chunk.columns.difference(['_pushedToBQ','_jobEntryDateTime','_searchTermLink'])]
         chunk = chunk[chunk['platformId'] != 'platformId']
         chunk.to_csv("temp.csv", index=False)
         append_to_bq(bq_key, "crowdtangle."+tablename+"_expandedLinks", "temp.csv", schema=expanded_links_schema)
 
-    for chunk in pd.read_sql_query("SELECT * FROM "+tablename+"_history WHERE _jobEntryDateTime=\""+job_id+"\"", conn, chunksize=chunksize):
+    # count = 0
+    for chunk in pd.read_sql_query("SELECT * FROM "+tablename+"_history", conn, chunksize=chunksize):
+    # for chunk in pd.read_sql_query("SELECT * FROM "+tablename+"_history WHERE _jobEntryDateTime=\""+job_id+"\"", conn, chunksize=chunksize):
+        # count += 50000
+        # if count > 123200000:
         chunk = chunk[chunk.columns.difference(['_pushedToBQ','_jobEntryDateTime','_searchTermLink'])]
         chunk = chunk[chunk['platformId'] != 'platformId']
         chunk.to_csv("temp.csv", index=False)
         append_to_bq(bq_key, "crowdtangle."+tablename+"_history", "temp.csv", schema=history_schema)
+        # else:
+        #     print("skip")
 
-    sqlc.execute("DELETE FROM "+tablename+" WHERE _jobEntryDateTime = '"+job_id+"'")
-    sqlc.execute("DELETE FROM "+tablename+"_media WHERE _jobEntryDateTime = '"+job_id+"'")
-    sqlc.execute("DELETE FROM "+tablename+"_expandedLinks WHERE _jobEntryDateTime = '"+job_id+"'")
-    sqlc.execute("DELETE FROM "+tablename+"_history WHERE _jobEntryDateTime = '"+job_id+"'")
+
+    # sqlc.execute("DELETE FROM "+tablename+" WHERE _jobEntryDateTime = '"+job_id+"'")
+    # sqlc.execute("DELETE FROM "+tablename+"_media WHERE _jobEntryDateTime = '"+job_id+"'")
+    # sqlc.execute("DELETE FROM "+tablename+"_expandedLinks WHERE _jobEntryDateTime = '"+job_id+"'")
+    # sqlc.execute("DELETE FROM "+tablename+"_history WHERE _jobEntryDateTime = '"+job_id+"'")
     conn.commit()
-
